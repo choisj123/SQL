eb9d98178157189dd1119e48b0d87b54fb629376
choisj123 <98chltnwls@gmail.com>
2023-08-09 14:04:22 +0900

    원격 접속

--- MySQL/12.권한 설정, 역할 설정, MySQL 원격접속 설정.sql
+++ MySQL/12.권한 설정, 역할 설정, MySQL 원격접속 설정.sql
@@ -1,8 +1,16 @@
 use mysql;
+
+show tables;
+desc user;
+
 select user, host from user;
+
 -- 로컬시스템/외부 유저 등록
 -- 5.7 이후
 CREATE USER 'sampleUser'@'localhost' IDENTIFIED BY 'sampleUser1234!';
+CREATE USER 'sampleUser'@'%' IDENTIFIED BY 'sampleUser1234!';
+CREATE USER 'sampleUser'@'192.168.0.%' identified by 'sampleUser1234!';
+
 
 -- 예전 방식
 insert into user(host, user, password, ssl_ciper, x509_isuser, x_509_subject)
@@ -16,15 +24,24 @@ drop user sampleUser@localhost;
 flush privileges;
 
 -- 예제
-show grants for sampleUser@localhost;
+show grants for 'sampleUser'@'localhost';
 
 -- sampleUser(localhost/로컬랜/인터넷전체)의 접속을 허용하시오
-grant all on world.* to sampleUser@localhost identified by 'sampleUser1234!';
-grant all on *.* to sampleUser@'192.168.0.%' identified by 'sampleUser1234!';
-grant all on world.* to sampleUser@% identified by 'sampleUser1234!';
+grant all on world.* to 'sampleUser'@'localhost';
+grant all on *.* to 'sampleUser'@'192.168.0.%';
+grant all on world.* to 'sampleUser'@'%';
 
-grant select, insert on world.* to sampleUser@localhost identified by 'sampleUser1234!';
-grant update(Name) on world.city to sampleUser@localhost identified by 'sampleUser1234!';
+grant select, insert on world.* to sampleUser@localhost;
+grant update(Name) on world.city to sampleUser@localhost;
 
-revoke all on *.* from sampleUser@localhost;
+revoke all on *.* from 'sampleUser'@'localhost';
+
+
+-- 역할 설정
+
+CREATE ROLE world_admin;
+GRANT INSERT, DELETE ON world.city TO world_admin;
+GRANT world_admin TO sampleUser;
+
+DROP ROLE world_admin;
 
